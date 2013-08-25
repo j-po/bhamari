616a70bd4cb5b19106f49b097ddfc2dc1c38000a
J Polin <jonathan.polin@gmail.com>
2013-08-25 12:24:48 -0700

    Fixed ordering mistake with iso_to_epoch().

--- asana_reporter.py
+++ asana_reporter.py
@@ -3,6 +3,10 @@ import requests
 import time
 from calendar import timegm
 
+def iso_to_epoch(iso_time):
+    time_obj=time.strptime(iso_time[:-5],'%Y-%m-%dT%H:%M:%S')
+    return timegm(time_obj)
+
 user_info=json.load(open('user_info.json', 'r'))
 asana_api_key = user_info['asana_api_key']
 asana_url = 'https://app.asana.com/api/1.0/'
@@ -12,7 +16,6 @@ beeminder_username= user_info['beeminder_username']
 goal_name = user_info['goal_name'] 
 beeminder_auth_token= user_info['beeminder_auth_token'] 
 beeminder_url = 'https://www.beeminder.com/api/v1/users/' + beeminder_username + '/goals/' + goal_name
-current_time=time.time()
 
 # Get ids of tagged tasks
 asana_request=requests.get(asana_url + tag_url, auth=(asana_api_key,'')).json()
@@ -29,8 +32,3 @@ for t in tasks:
 beeminder_payload={'datapoints':json.dumps([{"timestamp": iso_to_epoch(t['data']['completed_at']), "value":1, "comment":t['data']['name']} for t in tasks if t['data']['completed']]), 'auth_token': beeminder_auth_token}
 
 r=requests.post(beeminder_url + '/datapoints/create_all.json', data=beeminder_payload)
-
-def iso_to_epoch(iso_time):
-    time_obj=time.strptime(iso_time[:-5],'%Y-%m-%dT%H:%M:%S')
-    return timegm(time_obj)
-
